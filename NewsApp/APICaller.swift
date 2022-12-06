0b157a92821b426e24e685ec9c95a131500d15c0
okumusefe <is.efeokumus@gmail.com>
2022-12-07 01:48:19 +0300

    test

---
+++ NewsApp/APICaller.swift
@@ -0,0 +1,64 @@
+//
+//  APICaller.swift
+//  NewsApp
+//
+//  Created by Efe Okumuş on 6.12.2022.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3b6abf7a5d2b472b8ee415a68c9499c9")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+            
+        }
+        
+        task.resume()
+    }
+}
+
+// Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
