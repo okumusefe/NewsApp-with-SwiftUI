2c0f56e0a665c23e5bf8769e862c36879ef11d0c
okumusefe <is.efeokumus@gmail.com>
2022-12-06 14:09:26 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Efe Okumuş on 6.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
